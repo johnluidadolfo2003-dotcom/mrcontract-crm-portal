2249b1c83d5cbed52c2c049bb652b5cc974a70c6
johnluidadolfo2003-dotcom <johnluidadolfo2003@gmail.com>
2026-09-17 02:10:31 +0800

    Make builds repeatable and remove temporary artifacts

--- fix_buttons.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/components/ui/LeadDrawer.tsx', 'utf8');
-content = content.replace(/<\/div>(\s*(?:type=|onClick=|disabled=|className=))/g, '<button$1');
-fs.writeFileSync('src/components/ui/LeadDrawer.tsx', content);
--- fix_select.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/components/ui/LeadDrawer.tsx', 'utf8');
-content = content.replace('</select></div>', '</select>');
-fs.writeFileSync('src/components/ui/LeadDrawer.tsx', content);
